a691e7e83dc0c69a4189c4f4d35e29ce6230d67e
Javi <41807064+rjgi7@users.noreply.github.com>
2026-09-18 16:02:58 -0400

    Refresh PWA cache for audio and PIN update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'my-stars-v2';
+const CACHE = 'my-stars-v3';
 const SHELL = ['/', '/index.html', '/styles.css', '/app.js', '/config.js', '/manifest.webmanifest', '/icons/icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
